74e1276b74d8cb8605fe2759a84dbd439a1ab177
Manichand9848 <gaddammanichand0@gmail.com>
2026-03-11 23:14:14 +0530

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,6 +1,11 @@
 // js/firebase.js
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyD8WZGgK37nKph5gfOL-cjbtdo-doEEiWc",
@@ -12,4 +17,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
+
+export const db = getFirestore(app);
